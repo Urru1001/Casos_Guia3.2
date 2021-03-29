1d188a85c1925434e864bc99ef38acd4ac95751a
USER <USER@USER-PC>
2021-03-28 22:01:55 -0500

    Casos_Guia3.2 L.P.

--- src/caso3/caso10.java
+++ src/caso3/caso10.java
@@ -1,104 +1,97 @@
 package caso3;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class caso10 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int nr = 0, i = 0, ht = 0, tf = 0, emp90 = 0;
-		float bono = 0, desc = 0;
-		String continuar = "S";
+        Scanner sc = new Scanner(System.in);
 		
-		String [] nombre = new String [nr];
-		double [] sueldo_n = new double [nr];
-		double [] min_t = new double [nr];
-		
-		String nom_mayor = sc.nextLine();
-		String nom_menor = sc.nextLine();
-		double sn_mayor = sc.nextDouble();
-		double sn_menor = sc.nextDouble();
-		double min_t_mayor = sc.nextDouble();
+		String continuar="S", nom_1="", nom_2="", tar_nom = "";
+		int nr = 0, porc = 0, min_mayor = 0;
+		float s_menor = 0, s_mayor = 0;
 		
 		while (continuar.equals("S") || continuar.equals("s")) {
 			nr++;
 			System.out.println("---------------------------");
-			System.out.println("---- R e g i s t r o [" + nr + "]----");
+			System.out.println("Registro de Empleados (" + nr + ")");
 			System.out.println("---------------------------");
-			System.out.println("Nombre de alumno: ");
-			nombre [i] = sc.nextLine();
-			System.out.println("Horas trabajadas: ");
-			ht = sc.nextInt();
-			System.out.println("Tarifa por hora");
-			tf =sc.nextInt();
-			System.out.println("Minutos de tardanza: ");
-			min_t [i] = sc.nextDouble();
-			
-			int sueldo_b = ht * tf;
-			
-			if (ht <= 50)
-				bono = 0;
-			else if (ht > 50 && ht <= 60)
-				bono = sueldo_b * 0.02f;
-			else if (ht > 60 && ht <= 70)
-				bono = sueldo_b * 0.08f;
-			else if (ht > 70 && ht <= 80)
-				bono = sueldo_b * 0.13f;
-			else
-				bono = sueldo_b * 0.15f;
-			
-			if (min_t[i] <= 15)
-				desc = 0;
-			else if (min_t[i] > 15 && min_t[i] <= 30)
-				desc = (float)(sueldo_b * min_t[i] * 0.003);
-			else
-				desc = (float)(sueldo_b * min_t[i] * 0.005);
-			
-			sueldo_n [i] = sueldo_b + bono - desc;
-			float porc_h = (ht * 5) / 4;
-			if (porc_h > 90)
-				emp90++;
-			
-			if (sueldo_n [i] > sn_mayor) {
-				sn_mayor = sueldo_n[i];
-				nom_mayor = nombre[i];
-			}
-			else
-				sn_menor = sueldo_n[i];
-			if(sueldo_n [i] <  sn_menor) {
-				sn_menor = sueldo_n[i];
-			    nom_menor = nombre[i];
+			System.out.print("Empleado: ");
+			String nombre = sc.nextLine();
+			System.out.print("Horas trabajadas: ");
+			float ht = sc.nextFloat();
+			System.out.print("Tarifa por hora: ");
+			float tarifa = sc.nextInt();
+			System.out.print("Minutos de tardanza: ");
+			int min = sc.nextInt();
+			System.out.println("\n--------------------");
+			System.out.println("     Resultados");
+			System.out.println("--------------------");
+
+			float s_bruto = ht * tarifa, bon = 0, des = 0;
+
+			if (nr == 1) {
+				
+				min_mayor = min;
 			}
 			
+			if (ht <= 50) 
+				bon = 0;
+			else if (ht > 50 & ht <= 60) 
+				bon = s_bruto * 0.02f;
+			else if (ht > 60 & ht <= 70) 
+				bon = s_bruto * 0.08f;
+			else if (ht > 70 & ht <= 80) 
+				bon = s_bruto * 0.13f;
+			else if (ht > 80) 
+				bon = s_bruto * 0.15f;
 			
+			if (min <= 15) 
+				des = 0;
+			else if (min > 15 & min <= 30) 
+				des = min * s_bruto * 0.003f;
+			else if (min > 30) 
+				des = min * s_bruto * 0.005f;
 			
-			System.out.println("========================");
-			System.out.println("==R E S U L T A D O S ==");
-			System.out.println("========================");
-			System.out.println("Sueldo bruto: " + sueldo_b);
-			System.out.println("Bonificacion: " + bono);
-			System.out.println("Descuento: " + desc);
-			System.out.println("Sueldo neto: " + sueldo_n);
-			System.out.println("% Alcanzado: " + porc_h + "%");
+			float meta = (ht * 100)/80;
+			float sue_net = s_bruto + bon - des;
+			System.out.println("Sueldo bruto: " + s_bruto);
+			System.out.println("Bonificación: " + bon);
+			System.out.println("Descuento: " + des);
+			System.out.println("Sueldo neto: " + sue_net );
+			System.out.println("% Alcanzado: " +  meta + "%");
+			sc.nextLine();
 			
-            sc.nextLine();
+			if (meta > 90) 
+				porc ++;
 			
-			System.out.println("¿Nuevo empleado? [S/N]: ");
+			if (min > min_mayor) 
+				min_mayor = min; tar_nom = nombre;
+
+			if (sue_net > s_mayor) {
+				s_mayor = sue_net;
+				nom_1 = nombre;
+			}
+			else if (sue_net > s_menor) {
+				s_menor = sue_net;
+				nom_2 = nombre;
+			}
+
+			System.out.println("-------------------------");
+			System.out.print("¿Nuevo empleado? [S/N]: ");
 			continuar = sc.nextLine();
 			
-
-	}
-		System.out.println("\n--- R E S U M E N ---");
-		System.out.println("Numero de empleados: " + nr);
-		System.out.println("Sueldo neto mayor: " + sn_mayor + "pertenece a " + nom_mayor);
-		System.out.println("Sueldo neto menor: " + sn_menor + "pertenece a " + nom_menor);
-		if (min_t[i] > min_t_mayor) {
-			min_t_mayor = min_t[i];
-			nom_mayor = nombre[i];
 		}
-		System.out.println("Empleado con la mayor cantidad de minutos de tardanza: " + nom_mayor + "(" + min_t_mayor + ")");
-		System.out.println("Cantidad de emplados con mas del 90% de la meta: " + emp90);
+
+		System.out.println("--------------------");
+		System.out.println("     Resumen");
+		System.out.println("--------------------");
+		System.out.println("Número de empleados: " + nr);
+		System.out.println("Sueldo neto mayor: " + s_mayor + " pertenece a " + nom_1);
+		System.out.println("Sueldo neto menor: " + s_menor + " pertenece a " + nom_2);
+		System.out.println("Empleado con más minutos de tardanza: " + tar_nom + " (" + min_mayor + " min.)");
+		System.out.println("Cantidad de empleados con más del 90% de la meta: " + porc);
 		
 
 	}
--- src/caso3/caso9.java
+++ src/caso3/caso9.java
@@ -1,6 +1,6 @@
 package caso3;
 
-
+import  java.text.DecimalFormat ;
 import java.util.Scanner;
 
 
@@ -9,19 +9,11 @@ public class caso9 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		float  asist = 0, por_asist = 0;
-		int i = 0, nr = 0, n1 = 0, n2 = 0, n3 = 0, si_cer = 0;
-		String estado = "", continuar = "S";
-		
-		String [] nombre = new String [nr];
-		float [] promedio = new float [nr];
-		
-		String nom_mayor = "";
-		String nom_menor = "";
-		float prom_mayor = 0;
-		float prom_menor = prom_mayor;
-		
+		DecimalFormat df =  new  DecimalFormat ( " # .00 " );
+		float  asist = 0, por_asist = 0, prom_mayor = 0, prom_menor = 0;
+		float nr = 0, n1 = 0, n2 = 0, n3 = 0, si_cer = 0;
+		String estado = "", continuar = "S", nom_mayor = "", nom_menor = "";
+	
 	
 		while (continuar.equals("S") || continuar.equals("s")) {
 			nr++;
@@ -29,7 +21,7 @@ public class caso9 {
 			System.out.println("---- R e g i s t r o [" + nr + "]----");
 			System.out.println("---------------------------");
 			System.out.println("Nombre de alumno: ");
-			nombre [i] = sc.nextLine();
+			String nombre = sc.nextLine();
 			System.out.println("Nota 1: ");
 			n1 = sc.nextInt();
 			System.out.println("Nota 2: ");
@@ -39,36 +31,39 @@ public class caso9 {
 			System.out.println("Asistencia [1-12]: ");
 			asist = sc.nextInt();
 			
-			promedio[i] = (float)((n1 * 0.2) + (n2 * 0.3) + (n3 * 0.5));
+			float promedio = (float)((n1 * 0.2) + (n2 * 0.3) + (n3 * 0.5));
 			por_asist = (float)((asist * 100) / 12);
 			
-			if (promedio [i] >= 13 && por_asist >= 70) {
+			if (promedio >= 13 && por_asist >= 70) {
 				estado = "Obtiene certificado";
 			    si_cer++;
 			}
 			else
 				estado = "Sin certificado";
 			
-			if (promedio[i] > prom_mayor) {
-				prom_mayor = promedio [i];
-				nom_mayor = nombre [i];
-			}
-			if (promedio[i] < prom_menor) {
-				prom_menor = promedio [i];
-				nom_menor = nombre [i];
-			}
+			if (nr == 1) {
+				if (promedio > prom_menor) {
+					prom_menor = promedio;
+					nom_menor = nombre;
+				}
+            }
 			        
 			
 			System.out.println("-----------------");
 			System.out.println("R E S U L T A D O S");
 			System.out.println("-----------------");
-			System.out.println("Promedio: " + promedio[i]);
-			System.out.println("Asistencia: " + por_asist + "%");
+			System.out.println("Promedio: " + promedio);
+			System.out.println("Asistencia: " + df.format(por_asist) + "%");
 			System.out.println("Estado: " + estado);
 			
             sc.nextLine();
+          
+				if (promedio > prom_mayor) {
+					prom_mayor = promedio;
+					nom_mayor = nombre;
+				}
 			
-			System.out.println("¿Registrar otro? [S/N]: ");
+			System.out.print("\n¿Registrar otro? [S/N]: ");
 			continuar = sc.nextLine();
 		}
 		
